1626f6cc423283bd8f879766bbfb0917519fa23e
Magnus Rødnes Lehne <magnurle@stud.ntnu.no>
2026-08-20 16:25:22 +0200

    load model and movie data

--- app.py
+++ app.py
@@ -22,3 +22,13 @@ def recommend(movie):
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
     return recommended_movie_names,recommended_movie_posters
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('model/movie_list.pkl','rb'))
+similarity = pickle.load(open('model/similarity.pkl','rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
